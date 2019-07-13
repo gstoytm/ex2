0d7c1ade12a724f0b83c7a3e29f76aa4af83b8d5
Chinnawat Witoon <59143484@g.mail.com>
2019-07-13 14:13:56 +0700

    เพิ่มtest

--- index.js
+++ index.js
@@ -6,4 +6,9 @@ app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/about', (req, res) => res.send('about'))
 app.get('/contact', (req, res) => res.send('contact'))
 
+app.get('/test', (req, res) => {
+        res.json({message: "test"})
+
+} )
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
